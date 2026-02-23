41da4aca0f4254f77814bf08351d497acc2c4cf2
eXaive <royan.reddie@hotmail.com>
2026-02-23 08:43:33 -0500

    Add components/TodoList.tsx

---
+++ components/TodoList.tsx
@@ -0,0 +1,34 @@
+import { Todo } from './TodoApp'
+import TodoItem from './TodoItem'
+
+interface TodoListProps {
+  todos: Todo[]
+  onToggle: (id: number) => void
+  onDelete: (id: number) => void
+}
+
+export default function TodoList({ todos, onToggle, onDelete }: TodoListProps) {
+  if (todos.length === 0) {
+    return (
+      <div className="text-center py-12">
+        <div className="text-6xl mb-4">📝</div>
+        <p className="text-gray-500 text-lg">
+          No tasks yet. Add one above to get started!
+        </p>
+      </div>
+    )
+  }
+
+  return (
+    <div className="space-y-3 mb-8">
+      {todos.map(todo => (
+        <TodoItem
+          key={todo.id}
+          todo={todo}
+          onToggle={onToggle}
+          onDelete={onDelete}
+        />
+      ))}
+    </div>
+  )
+}
